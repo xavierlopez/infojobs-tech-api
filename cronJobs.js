1375d553a0a0113290c7052b080cf62e460d7d65
Xavier L <lopezx@gmail.com>
2023-05-22 18:10:50 +0200

    added cronjob for database feeding

--- cronJobs.js
+++ cronJobs.js
@@ -1,11 +1,8 @@
 const gs = require('./services/generalService');
-
+const cron = require('node-cron');
 
 // execute cron task (Database feeding)
-async function executeservice() {
-    
+cron.schedule('* * * * *', async () => {
     console.log('Ejecutando tarea cada minuto');
     await gs.databasefeed();
-}
-
-executeservice();
+});
