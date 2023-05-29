7ad10a3d01101474b35460a3d40b49a8a05a880a
Xavier L <lopezx@gmail.com>
2023-05-29 21:03:16 +0200

    limit 60 offers

--- services/infojobsService.js
+++ services/infojobsService.js
@@ -1,5 +1,5 @@
 async function getITOffers()  {
-    const url_ofertas = 'https://api.infojobs.net/api/9/offer?category=informatica-telecomunicaciones&maxResults=50';
+    const url_ofertas = 'https://api.infojobs.net/api/9/offer?category=informatica-telecomunicaciones&maxResults=5';
     const headers = {
         'Authorization': `Basic ${process.env.API_KEY}`,
       };
