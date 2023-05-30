ea1cf9be0502940c24cfa832d2d07877482dc92e
Xavier L <lopezx@gmail.com>
2023-05-30 08:47:10 +0200

    limit readjusted to 55

--- models/offer.model.js
+++ models/offer.model.js
@@ -120,7 +120,7 @@ offerSchema.statics.simplifyOffer = (offer) => {
  */
 
 offerSchema.statics.getStackStatistics = async (stack_string) => {
-    let offers = await Offer.find({stack:stack_string}).limit(60);
+    let offers = await Offer.find({stack:stack_string}).limit(40);
    
     
     let skillsList = offers.reduce((skills, offer) => {
